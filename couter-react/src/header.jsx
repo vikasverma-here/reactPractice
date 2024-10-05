4ecf90590afe4a7b347912604d6f28bc50e42d34
vicky <vikasverma995619@gmail.com>
2024-10-05 17:22:57 +0530

    practice some css things

---
+++ couter-react/src/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='nav'>
+      <ul>
+        <li>Logo</li>
+       <div className="center">
+       <li>About us</li>
+        <li> Contact us  </li>
+        <li> Services</li>
+    
+       </div>
+       <button>Login</button>
+      </ul>
+    </div>
+  )
+}
+
+export default Header

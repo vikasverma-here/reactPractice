3bee96aa300d3299c1d94c3df487b2431a739f78
vicky <vikasverma995619@gmail.com>
2024-10-23 18:08:01 +0530

    practice redux toolkit

--- redux-first/src/Counter.jsx
+++ redux-first/src/Counter.jsx
@@ -4,14 +4,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement, reset } from './counterSlice';
 
 function Counter() {
-  const count = useSelector((state) => state.counter.value);
+  const count = useSelector((sta) => sta.counter.value);
   const dispatch = useDispatch();
 
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Counter: {count}</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onMouseMove={() => dispatch(increment())}>Increment</button>
+      <button onMouseMove={() => dispatch(decrement())}>Decrement</button>
       <button onClick={() => dispatch(reset())}>Reset</button>
     </div>
   );
